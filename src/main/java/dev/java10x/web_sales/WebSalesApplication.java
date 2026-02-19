02d92841d7afef39b01def639241980d2536fd23
c250368 <c250368@ccg.ac.jp>
2026-02-19 17:07:01 +0900

    First commit

---
+++ src/main/java/dev/java10x/web_sales/WebSalesApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.web_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/web_sales/WebSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.web_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
